8df84f059fd3a1791ce8cf18ba01f555e1de1ba8
Benson Che <bche@resdat.com>
2021-06-10 00:33:32 -0700

    Add caching of credentials

--- ImageFilter/Controllers/ImageFilterController.cs
+++ ImageFilter/Controllers/ImageFilterController.cs
@@ -1,5 +1,6 @@
 ﻿using ImageFilter.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SixLabors.ImageSharp;
@@ -17,11 +18,14 @@ namespace ImageFilter.Controllers
         private readonly ILogger<ImageFilterController> _logger;
         private static HttpClient client = new HttpClient();
 
+        private IMemoryCache _cache;
+
         private const string _serverName = "http://192.168.1.108:5000";
 
-        public ImageFilterController(ILogger<ImageFilterController> logger)
+        public ImageFilterController(ILogger<ImageFilterController> logger, IMemoryCache memoryCache)
         {
             _logger = logger;
+            _cache = memoryCache;
         }
 
         internal class SnapshotErrorResponse
@@ -46,6 +50,14 @@ namespace ImageFilter.Controllers
         [Route("WebApi/auth.cgi")]
         public async Task<JsonResult> Authenticate(string account, string passwd)
         {
+            if (account == null)
+                account = "";
+            if (passwd == null)
+                passwd = "";
+
+            _cache.Set("username", account);
+            _cache.Set("password", passwd);
+
             var authUrl = $"{_serverName}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=1&account={account}&passwd={passwd}&session=SurveillanceStation";
             var response = await client.GetAsync(authUrl);
             if (response.IsSuccessStatusCode)
@@ -63,38 +75,54 @@ namespace ImageFilter.Controllers
         }
 
         [Route("WebApi/entry.cgi")]
-        public async Task<IActionResult> GetImage(int cameraId)
+        public async Task<IActionResult> GetImage(int cameraId, bool allowRecursion = true)
         {
             var snapshotUrl = $"{_serverName}/webapi/entry.cgi?camStm=1&version=2&cameraId={cameraId}&api=%22SYNO.SurveillanceStation.Camera%22&method=GetSnapshot";
 
             var response = await client.GetAsync(snapshotUrl);
             if (response.IsSuccessStatusCode)
             {
-                using (var image = await Image.LoadAsync(await response.Content.ReadAsStreamAsync()))
+                try
                 {
-                    var polygon = new Polygon(new LinearLineSegment(
-                        new PointF(0, 0),
-                        new PointF((float)(image.Width * 0.76), 0),
-                        new PointF((float)(image.Width * 0.79), 400),
-                        new PointF((float)(image.Width * 0.83), 430),
-                        new PointF((float)(image.Width * 0.82), 840),
-                        new PointF((float)(image.Width * 0.83), 870),
-                        new PointF((float)(image.Width * 0.83), 990),
-                        new PointF((float)(image.Width * 0.31), 1030),
-                        new PointF(0, 1335)
-                        ));
-
-                    image.Mutate(x => x.Fill(Color.Black, polygon));
-
-                    using (var stream = new MemoryStream())
+                    using (var image = await Image.LoadAsync(await response.Content.ReadAsStreamAsync()))
                     {
-                        await image.SaveAsJpegAsync(stream);
+                        var polygon = new Polygon(new LinearLineSegment(
+                            new PointF(0, 0),
+                            new PointF((float)(image.Width * 0.76), 0),
+                            new PointF((float)(image.Width * 0.79), 400),
+                            new PointF((float)(image.Width * 0.83), 430),
+                            new PointF((float)(image.Width * 0.82), 840),
+                            new PointF((float)(image.Width * 0.83), 870),
+                            new PointF((float)(image.Width * 0.83), 990),
+                            new PointF((float)(image.Width * 0.31), 1030),
+                            new PointF(0, 1335)
+                            ));
 
-                        var content = stream.ToArray();
+                        image.Mutate(x => x.Fill(Color.Black, polygon));
 
-                        return File(content, "image/jpeg", "snapshot.jpeg");
+                        using (var stream = new MemoryStream())
+                        {
+                            await image.SaveAsJpegAsync(stream);
+
+                            var content = stream.ToArray();
+
+                            return File(content, "image/jpeg", "snapshot.jpeg");
+                        }
                     }
                 }
+                catch
+                {
+                    if (allowRecursion)
+                    {
+                        await Authenticate((string)_cache.Get("username"), (string)_cache.Get("password"));
+                        return await GetImage(cameraId, false);
+                    }
+                }
+            }
+            else if (allowRecursion)
+            {
+                await Authenticate((string)_cache.Get("username"), (string)_cache.Get("password"));
+                return await GetImage(cameraId, false);
             }
 
             return null;
